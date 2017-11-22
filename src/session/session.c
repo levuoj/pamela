4731710189080776dc69fb2a5a67c197bf5a5c5a
levuoj <anthony.jouvel@epitech.eu>
2017-11-22 06:00:43 -0500

    test password

--- src/session/session.c
+++ src/session/session.c
@@ -5,9 +5,10 @@
 ** Login   <anthony.jouvel@epitech.eu>
 **
 ** Started on  Tue Nov 21 18:45:54 2017 Jouvel Anthony
-** Last update Tue Nov 21 21:40:41 2017 Jouvel Anthony
+** Last update Wed Nov 22 05:55:14 2017 pamela
 */
 
+#include <security/pam_ext.h>
 #include "session.h"
 #include "unused.h"
 
@@ -16,11 +17,17 @@ int		pam_sm_open_session(pam_handle_t *pamh,
 				    int argc,
 				    const char **argv)
 {
-  UNUSED(pamh);
   UNUSED(flags);
   UNUSED(argc);
   UNUSED(argv);
-  return (PAM_IGNORE);
+  const char	*password = NULL;
+  int		ret_value;
+
+  if ((ret_value = pam_get_authtok(pamh, PAM_AUTHTOK, &password, NULL)) != PAM_SUCCESS ||
+      password == NULL)
+    return (ret_value);
+  printf("password = %s\n", password);
+  return (PAM_SUCCESS);
 }
 
 int		pam_sm_close_session(pam_handle_t *pamh,
