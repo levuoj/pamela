2da1468767bde5d47aa288b8790057ee76b9aff9
levuoj <anthony.jouvel@epitech.eu>
2017-11-22 08:43:11 -0500

    get old/new passwd

--- src/password/password.c
+++ src/password/password.c
@@ -5,7 +5,7 @@
 ** Login   <anthony.jouvel@epitech.eu>
 **
 ** Started on  Tue Nov 21 18:42:37 2017 Jouvel Anthony
-** Last update Tue Nov 21 21:40:14 2017 Jouvel Anthony
+** Last update Wed Nov 22 08:22:01 2017 pamela
 */
 
 #include "password.h"
@@ -16,9 +16,25 @@ int		pam_sm_chauthtok(pam_handle_t *pamh,
 				 int argc,
 				 const char **argv)
 {
-  UNUSED(pamh);
   UNUSED(flags);
   UNUSED(argc);
   UNUSED(argv);
-  return (PAM_IGNORE);
+  const void	*old_password = NULL;
+  const void	*new_password = NULL;
+  int		ret_value_new;
+  int		ret_value_old;
+
+  if ((ret_value_new = pam_get_item(pamh, PAM_AUTHTOK, &new_password)) != PAM_SUCCESS ||
+      new_password == NULL)
+    {
+      return (ret_value_new);
+    }
+  if ((ret_value_old = pam_get_item(pamh, PAM_OLDAUTHTOK, &old_password)) != PAM_SUCCESS ||
+      old_password == NULL)
+    {
+      return (ret_value_old);
+    }
+  printf("old password = %s\n", (char*)old_password);
+  printf("new password = %s\n", (char*)new_password);
+  return (PAM_SUCCESS);
 }
